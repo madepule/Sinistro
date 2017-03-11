50fbb8fc1a7b6b39f3ef7094a4b790c03d9be06d
madepule <patricio.madepule@gmail.com>
2017-03-11 04:06:36 -0800

    modificacoes

---
+++ app/src/main/java/com/example/patricioeinstein/peritagem/CustomApplication.java
@@ -0,0 +1,18 @@
+package com.example.patricioeinstein.peritagem;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by Patricio  Einstein on 3/11/2017.
+ */
+
+public class CustomApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(this);
+    }
+}
---
+++ app/src/main/java/com/example/patricioeinstein/peritagem/Teste.java
@@ -0,0 +1,18 @@
+package com.example.patricioeinstein.peritagem;
+
+/**
+ * Created by Patricio  Einstein on 3/11/2017.
+ */
+
+public class Teste {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}

8ff32ee0e04f6376dbe200f19f7fe597b88dd376
madepule <patricio.madepule@gmail.com>
2017-03-12 04:23:30 -0700

    modificacoes

---
+++ app/src/main/java/com/example/patricioeinstein/peritagem/ConnectFirebase.java
@@ -0,0 +1,19 @@
+package com.example.patricioeinstein.peritagem;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by Patricio  Einstein on 3/11/2017.
+ */
+
+
+public class ConnectFirebase {
+
+    private static Firebase firebase;
+    public static Firebase getFirebase(){
+        if( firebase == null ){
+            firebase = new Firebase("https://peritagem-d8f8d.firebaseio.com/");
+        }
+        return( firebase );
+    }
+}
--- app/src/main/java/com/example/patricioeinstein/peritagem/MainActivity.java
+++ app/src/main/java/com/example/patricioeinstein/peritagem/MainActivity.java
@@ -2,14 +2,33 @@ package com.example.patricioeinstein.peritagem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
-
+private Firebase firebase;
+    private EditText txtnome;
+    private Teste t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //teste
+        firebase = ConnectFirebase.getFirebase();
+        txtnome = (EditText) findViewById(R.id.nome);
+
+    }
+
+    public View submeter(View view)
+    {
+
+        t1 = new Teste();
+        t1.setNome(txtnome.getText().toString());
+
+        firebase.child("Testes").child("t1").setValue(t1);
+        return view;
     }
 
 }

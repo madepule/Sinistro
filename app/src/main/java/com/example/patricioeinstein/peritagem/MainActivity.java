fdd8e3d7c3c5580d83d6db8e6b583c996bb587c7
madepule <patricio.madepule@gmail.com>
2017-03-13 02:13:09 -0700

    modificacoes

--- app/src/main/java/com/example/patricioeinstein/peritagem/MainActivity.java
+++ app/src/main/java/com/example/patricioeinstein/peritagem/MainActivity.java
@@ -15,11 +15,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         //teste
        firebase = ConnectFirebase.getFirebase();
 
-        txtnome = (EditText) findViewById(R.id.nome);
+        //txtnome = (EditText) findViewById(R.id.nome);
 
     }
 

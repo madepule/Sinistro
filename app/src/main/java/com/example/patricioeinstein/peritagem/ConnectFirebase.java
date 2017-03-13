215c106da6b9d310d4fc7c8d427a2786ab05c2af
madepule <patricio.madepule@gmail.com>
2017-03-13 04:31:23 -0700

    modificacoes

--- app/src/main/java/com/example/patricioeinstein/peritagem/ConnectFirebase.java
+++ app/src/main/java/com/example/patricioeinstein/peritagem/ConnectFirebase.java
@@ -7,15 +7,16 @@ import com.firebase.client.Firebase;
  * Created by Patricio  Einstein on 3/11/2017.
  */
 
-
+//esta classe serve para conectar o A.Studio com o FireBase
 public class ConnectFirebase {
 
     private static Firebase firebase;
+
     public static Firebase getFirebase(){
        if( firebase == null ){
           firebase = new Firebase("https://peritagem-d8f8d.firebaseio.com/");
         }
-       return( firebase );
+      return( firebase );
    }
 
 }
--- app/src/main/java/com/example/patricioeinstein/peritagem/MainActivity.java
+++ app/src/main/java/com/example/patricioeinstein/peritagem/MainActivity.java
@@ -1,36 +1,47 @@
 package com.example.patricioeinstein.peritagem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
 import com.firebase.client.Firebase;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class MainActivity extends AppCompatActivity {
+    //instanciar a classe connectFirebas
     private Firebase firebase;
     private EditText txtnome;
     private Teste t1;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //teste
-       firebase = ConnectFirebase.getFirebase();
+        firebase = ConnectFirebase.getFirebase();
 
         txtnome = (EditText) findViewById(R.id.nome);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public View submeter(View view)
-    {
+    public View submeter(View view) {
 
         t1 = new Teste();
         t1.setNome(txtnome.getText().toString());
 
-        firebase.child("Testes").child("t1").setValue(t1);
+       // firebase.child("Testes").child("t1").setValue(t1);
         return view;
     }
 
